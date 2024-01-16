69acbd81fbd12abec9aefe3acc6ee46137c091c1
camiiku <113323941+syahmi1110@users.noreply.github.com>
2024-01-16 21:37:53 +0800

    Create Server C Socket.c
    ITT440 SKILLBASED

---
+++ Server C Socket.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <time.h>
+
+int main() {
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+    int random_number;
+
+    // Create a socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Forcefully attaching socket to the port 8080
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt>
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(8080);
+
+    // Bind the socket to the address
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+
+
+
+    // Listen for incoming connections
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+while(1){
+    // Accept a connection
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrl>
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+
+    // Generate a random number
+    srand(time(NULL));
+    random_number = rand() % 100;
+
+    // Send the random number to the client
+    sprintf(buffer, "%d", random_number);
+    send(new_socket, buffer, strlen(buffer), 0);
+
+    close(new_socket);
+
+}
+    // Close the sockets
+    close(server_fd);
+    return 0;
+}
